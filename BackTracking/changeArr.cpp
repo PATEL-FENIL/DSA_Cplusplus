ee382221e64fd95399990324ea9014e725e9aa80
PATEL FENIL DINESHKUMAR <141541284+PATEL-FENIL@users.noreply.github.com>
2024-12-19 19:39:03 +0530

    Update changeArr.cpp

--- BackTracking/changeArr.cpp
+++ BackTracking/changeArr.cpp
@@ -1,25 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-// void changeArr(vector<int>&arr,int idx,int val)
-// {
-//     if(idx == arr.size())
-//     {
-//         for(int i = 0 ; i < arr.size() ; i++)
-//         {
-//             cout << arr[i] << " ";
-//         }
-//         return;
-//     }
-//     arr[idx] = val;
-//     changeArr(arr,idx + 1,val + 1);
-//     arr[idx] = arr[idx] - 2;
-// }
-// int main()
-// {
-//     vector<int>arr(5,0);
-//     changeArr(arr,0,1);
-//     return 0;
-// }
 class Solution
 {
     public :
